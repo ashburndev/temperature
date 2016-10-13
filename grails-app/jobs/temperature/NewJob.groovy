a68827a6ba6f027d823f92c004c5959bc7c52816
David Holberton <david.holberton@gmail.com>
2016-10-13 08:41:11 -0400

    add new quartz job from command line

---
+++ grails-app/jobs/temperature/NewJob.groovy
@@ -0,0 +1,13 @@
+package temperature
+
+
+
+class NewJob {
+    static triggers = {
+      simple repeatInterval: 5000l // execute job once in 5 seconds
+    }
+
+    def execute() {
+        // execute job
+    }
+}
